ecec455d8e9246f2dcfdd2d3c28c61691404a1bc
Nick <nick@llamadigital.net>
2014-03-15 17:36:40 +0000

    path tests

--- app/models/bityee/page.rb
+++ app/models/bityee/page.rb
@@ -10,11 +10,12 @@ class Bityee::Page
   has_and_belongs_to_many :sections, class_name: 'Bityee::Section'
 
   validates :name, :path, presence: true, uniqueness: true
+  validates :path, format: {with: /\A[\w_-]+\Z/, message: 'can only contain letters and numbers (spaces should be denoted with - or _)'}
 
   before_validation :set_path
 
   def set_path
-    
+        
   end
 
 end
--- spec/models/bityee/page_spec.rb
+++ spec/models/bityee/page_spec.rb
@@ -20,10 +20,30 @@ module Bityee
         expect(subject).to_not be_valid
       end
 
-      it 'requires a path' do
-        subject.path = nil
-        expect(subject).to_not be_valid
+      context 'path' do
+        it 'requires a path' do
+          subject.path = nil
+          expect(subject).to_not be_valid
+        end
+
+        it 'requires a unique path' do
+          FactoryGirl.create(:bityee_page, path: 'bar')
+          subject.path = 'bar'
+          expect(subject).to_not be_valid
+        end
+        
+        ['string', 'String', 'String1', 'some-string', 'some_string'].each do |path|
+          it { expect(FactoryGirl.build(:bityee_page, path: path)).to be_valid }
+        end
+
+        ['String Gap', 's!llyp@th'].each do |path|
+          it { expect(FactoryGirl.build(:bityee_page, path: path)).to_not be_valid }
+        end
       end
+    end
+    
+    describe '#set_path' do
+      it 'should do things'
     end
   end
 end
